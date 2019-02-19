a1c2ce2797b4789ec25005a119b2f8fbc8264753
Edon <edon@192.168.0.102>
2019-02-19 22:28:00 +0800

    first commit

---
+++ pages/search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="search">
+    <ul v-show="list">
+      <li v-for="(item, index) in list" :key="index">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      list: []
+    }
+  },
+  // async created() {
+  //   let {status, data: {list}} = await axios.get('http://localhost:3000/city/list')
+  //   if (status === 200) {
+  //     this.list = list
+  //   }
+  // }
+  async asyncData() {
+    let {status, data: {list}} = await axios.get('http://localhost:3000/city/list')
+    if (status === 200) {
+      return {
+        list
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
